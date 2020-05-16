a796ac14b05c5fef382f2a5b7e4469130ee20f63
yshr10ic <yshr10ic@gmail.com>
2020-05-16 10:38:23 +0900

    インデックスページの作成

--- QuestionTime/config/urls.py
+++ QuestionTime/config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django_registration.backends.one_step.views import RegistrationView
 
+from core.views import IndexTemplateView
 from users.forms import CustomUserForm
 
 urlpatterns = [
@@ -16,4 +17,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    re_path(r'^.*$', IndexTemplateView.as_view(), name='entry-point')
 ]
