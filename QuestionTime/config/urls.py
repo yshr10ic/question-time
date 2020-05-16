3499bac56988cbca01f8e904008c18159ed07b05
yshr10ic <yshr10ic@gmail.com>
2020-05-16 10:53:57 +0900

    CurrentUser APIの実装

--- QuestionTime/config/urls.py
+++ QuestionTime/config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
         ), name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('api/', include('users.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
