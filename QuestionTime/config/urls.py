b6c498a941977e7d5dfa8b1e4fc7a370a92747e4
yshr10ic <yshr10ic@gmail.com>
2020-05-16 10:23:25 +0900

    URLの順番を変更（順番が違うとaccounts/register/でエラーとなる）

--- QuestionTime/config/urls.py
+++ QuestionTime/config/urls.py
@@ -6,13 +6,13 @@ from users.forms import CustomUserForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/register/',
         RegistrationView.as_view(
             form_class=CustomUserForm,
             success_url='/',
         ), name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
