a84324c299779cc1241b3e283a9c78509177f48a
yshr10ic <yshr10ic@gmail.com>
2020-05-21 21:27:08 +0900

    Answerビューセットの実装

--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -7,5 +7,11 @@ router = DefaultRouter()
 router.register(r'questions', qv.QuestionViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('questions/<slug:slug>/answers/',
+         qv.QuestionAnswerListAPIView.as_view(),
+         name='answer-list'),
+    path('questions/<slug:slug>/answer/',
+         qv.AnswerCreateAPIView.as_view(),
+         name='create-answer')
 ]
